6a0e3ffc9c5e4376089a45cab54b49efee206173
Bishal Bista <bishal.bista@webpoint.io>
2023-07-19 12:19:31 +0545

    feat: added common layout

---
+++ component/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex h-screen bg-white flex-col items-center gap-y-10">
+      <Navbar />
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
