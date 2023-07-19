b21851ab6a46757e02ec707486bc7087d2f02fef
Bishal Bista <bishal.bista@webpoint.io>
2023-07-19 12:20:53 +0545

    feat: added ssg and isr page

---
+++ pages/isr.js
@@ -0,0 +1,32 @@
+import Card from "@/component/Card";
+import { fetchData } from "@/utils/fetch";
+
+const ISRPage = ({ data }) => {
+  return (
+    <div className="text-center text-gray-700 flex flex-col gap-4 ">
+      <h1 className="font-bold text-xl">SSG Page</h1>
+      <p className=" font-semibold text-sm">
+        This page is rendered using Incremental Static Regeneration (ISR).
+      </p>
+
+      <div className="mt-10 flex flex-grow gap-4">
+        {data.map((item) => (
+          <Card user={item} key={item.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const data = await fetchData("api/data");
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 10, // Revalidate the page every 10 seconds
+  };
+}
+
+export default ISRPage;
---
+++ pages/ssg.js
@@ -0,0 +1,31 @@
+import Card from "@/component/Card";
+import { fetchData } from "@/utils/fetch";
+
+const SSGPage = ({ data }) => {
+  return (
+    <div className="text-center text-gray-700 flex flex-col gap-4 ">
+      <h1 className="font-bold text-xl">SSG Page</h1>
+      <p className=" font-semibold text-sm">
+        This page is rendered using Static Site Generation (SSG).
+      </p>
+
+      <div className="mt-10 flex flex-grow gap-4">
+        {data.map((item) => (
+          <Card user={item} key={item.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SSGPage;
+
+export async function getStaticProps() {
+  const data = await fetchData("/api/data");
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
