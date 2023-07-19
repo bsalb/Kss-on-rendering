99c269fb5ba82654a6f1280d40f7b65dfb27daeb
Bishal Bista <bishal.bista@webpoint.io>
2023-07-19 12:17:52 +0545

    feat: added routes

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const routes = [
+  {
+    link: "/",
+    name: "Home",
+  },
+  {
+    link: "/csr",
+    name: "CSR",
+  },
+  {
+    link: "/ssr",
+    name: "SSR",
+  },
+  {
+    link: "/ssg",
+    name: "SSG",
+  },
+  {
+    link: "/isr",
+    name: "ISR",
+  },
+];
+
+export default routes;
